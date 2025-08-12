841da8ac3d3c20db4f87c7512a3b4189bd40baea
NyxaSylph <vellure.hub@gmail.com>
2025-08-12 19:17:57 +0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -4,40 +4,34 @@ repeat task.wait() until game:IsLoaded() and game.Players.LocalPlayer and game.P
 local Players = game:GetService("Players")
 local HttpService = game:GetService("HttpService")
 
-for i,v in getconnections(Players.LocalPlayer.Idled) do
-    v:Disable()
+for _, Conn in getconnections(Players.LocalPlayer.Idled) do
+    Conn:Disable()
 end
 
 local Loader = {}
 
-Loader.Repository = "https://raw.githubusercontent.com/NyxaSylph/Vellure/refs/heads/main"
+Loader.Repository = "https://raw.githubusercontent.com/NyxaSylph/Vellure/main/"
 Loader.Files = {
-    ["A Universal Time"] = {
-        File = "AUT/Main.lua",
-        CreatorId = 6556072
-    },
-    ["Vox Seas"] = {
-        File = "VS/Main.lua", 
-        CreatorId = 35566755
-    }
+    ["A Universal Time"] = { File = "AUT/Main.lua", CreatorId = 6556072 },
+    ["Vox Seas"]         = { File = "VS/Main.lua",  CreatorId = 35566755 },
 }
 
-function Loader:LoadByCreatorId(creatorId)
+function Loader:LoadByCreatorId(CreatorId)
     for GameName, Data in pairs(self.Files) do
-        if Data.CreatorId == creatorId then
-            local url = self.Repository .. Data.File
-            local success, result = pcall(function()
-                return loadstring(game:HttpGet(url))()
+        if Data.CreatorId == CreatorId then
+            local Url = ("%s%s"):format(self.Repository, Data.File)
+            local Ok, Err = pcall(function()
+                return loadstring(game:HttpGet(Url))()
             end)
-            
-            if success then
+            if Ok then
                 print("✅ Loaded:", GameName)
             else
-                warn("❌ Everything went south :", GameName, result)
+                warn("❌ Everything went south:", GameName, Err)
             end
             return
         end
     end
+
     warn([[
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⢀⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣄⠀⠀⠀⠀⠀⠀
@@ -60,5 +54,4 @@ function Loader:LoadByCreatorId(creatorId)
 end
 
 Loader:LoadByCreatorId(game.CreatorId)
-
 return Loader

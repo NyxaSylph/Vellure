c950e2fb71b14b478f81e0ecd9e3edf44cd17f7c
NyxaSylph <vellure.hub@gmail.com>
2025-08-16 18:33:09 +0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,31 +1,45 @@
--- Made by @Flames9925 | Edited by @NyxaSylph
-repeat task.wait() until game:IsLoaded() and game.Players.LocalPlayer and game.Players.LocalPlayer.Character
+-- // Made by @Flames9925 | Edited by @NyxaSylph
+local cloneref = (cloneref or clonereference or function(instance: any)
+    return instance
+end)
 
-local Players = game:GetService("Players")
-local HttpService = game:GetService("HttpService")
+local HttpService: HttpService = cloneref(game:GetService("HttpService"));
+local Players: Players = cloneref(game:GetService("Players"));
+local LPlayer = Players.LocalPlayer;
 
-for _, Conn in getconnections(Players.LocalPlayer.Idled) do
-    Conn:Disable()
+repeat task.wait() until game:IsLoaded() and (LPlayer and LPlayer.Character)
+
+if typeof(getconnections) == "function" then
+    for _, Conn in ipairs(getconnections(LPlayer.Idled)) do
+        Conn:Disable()
+    end
+else
+    warn("❌ Anti AFK Failed")
 end
 
 local Loader = {}
 
-Loader.Repository = "https://raw.githubusercontent.com/NyxaSylph/Vellure/main/"
+Loader.Repositories = {["NyxaSylph"] = "https://raw.githubusercontent.com/NyxaSylph/Vellure/main/",
+	["FlamesW"]   = "https://raw.githubusercontent.com/FlamesW/Vellure/main/",
+}
+
 Loader.Files = {
-    ["A Universal Time"] = { File = "AUT/Main.lua", CreatorId = 6556072 },
-    ["Vox Seas"]         = { File = "VS/Main.lua",  CreatorId = 35566755 },
-    ["99nigt"]         = { File = "99nigt/Main.lua",  CreatorId = 6042520 },
+    ["A Universal Time"] = { Author = "NyxaSylph", File = "AUT/Main.lua", CreatorId = 6556072 },
+    ["Vox Seas"] = { Author = "NyxaSylph", File = "VS/Main.lua", CreatorId = 35566755 },
+    ["99nigt"] = { Author = "NyxaSylph", File = "99nigt/Main.lua", CreatorId = 6042520 },
+
+    ["Murim Cultivation"] = { Author = "FlamesW", File = "MC/Main.lua", CreatorId = 32416851 },
 }
 
 function Loader:LoadByCreatorId(CreatorId)
     for GameName, Data in pairs(self.Files) do
         if Data.CreatorId == CreatorId then
-            local Url = ("%s%s"):format(self.Repository, Data.File)
+            local Url = ("%s%s"):format(self.Repositories[Data.Author], Data.File)
             local Ok, Err = pcall(function()
                 return loadstring(game:HttpGet(Url))()
             end)
             if Ok then
-                print("✅ Loaded:", GameName)
+                print("✅ Loaded:", GameName, "by @" .. Data.Author)
             else
                 warn("❌ Everything went south:", GameName, Err)
             end

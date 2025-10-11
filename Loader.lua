8eb823e7fbe3e76732d88835ca045165520d952a
NyxaSylph <vellure.hub@gmail.com>
2025-10-11 16:57:15 +0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -29,6 +29,7 @@ Loader.Files = {
     ["99nigt"] = { Author = "NyxaSylph", File = "99nigt/Main.lua", CreatorId = 6042520 },
 	["Hunty Zombie"] = { Author = "NyxaSylph", File = "HuntyZ/main.lua", CreatorId = 36015593 },
 
+    ["Weak Legacy 2"] = { Author = "FlamesW", File = "WL2/Main.lua", CreatorId = 3096739365 },
     ["Murim Cultivation"] = { Author = "FlamesW", File = "MC/Main.lua", CreatorId = 32416851 },
 }
 
